74e13d3be55ecb8bb5170a62daffb506888ec031
Deepak Maram <deepak@mystenlabs.com>
2023-09-06 21:44:59 +0000

    Use environment variables

--- src/main_proofserver.cpp
+++ src/main_proofserver.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib> // for getenv
 #include <pistache/router.h>
 #include <pistache/endpoint.h>
 #include "proverapi.hpp"
@@ -8,21 +9,47 @@ using namespace CPlusPlusLogging;
 using namespace Pistache;
 using namespace Pistache::Rest;
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        std::cerr << "Invalid number of parameters:\n";
-        std::cerr << "Usage: proverServer <port> <circuit.zkey> \n";
-        return -1;
-    }
+void printUsage() {
+    std::cerr << "Invalid usage\n";
+    std::cerr << "Either use command line args: ./proverServer <zkLogin.zkey> <path_to_binaries>\n";
+    std::cerr << "Or set ZKEY and WITNESS_BINARIES env variables and call ./proverServer\n";
+}
 
+int main(int argc, char **argv) {
     Logger::getInstance()->enableConsoleLogging();
     Logger::getInstance()->updateLogLevel(LOG_LEVEL_INFO);
     LOG_INFO("Initializing server...");
-    int port = std::stoi(argv[1]); // parse port
-    // parse the zkey
-    std::string zkeyFileName = argv[2];
 
-    SingleProver prover(zkeyFileName);
+    int port = 8080;
+    LOG_INFO("Starting at port 8080");
+
+    std::string zkeyFileLoc;
+    std::string binariesFolderLoc;
+
+    if (argc == 3) {
+        zkeyFileLoc = argv[1];
+        binariesFolderLoc = argv[2];
+    } else if (argc == 1) {
+        LOG_INFO("Reading from environment variables ZKEY and WITNESS_BINARIES");
+        if (const char* x = getenv("ZKEY")) {
+            zkeyFileLoc = x;
+        } else {
+            printUsage();
+            return -1;
+        }
+
+        if (const char* x = getenv("WITNESS_BINARIES")) {
+            binariesFolderLoc = x;
+        } else {
+            printUsage();
+            return -1;
+        }
+    } else {
+        printUsage();
+        return -1;
+    }
+
+    SingleProver prover(zkeyFileLoc, binariesFolderLoc);
     ProverAPI proverAPI(prover);
     Address addr(Ipv4::any(), Port(port));
 
--- src/singleprover.hpp
+++ src/singleprover.hpp
@@ -17,13 +17,15 @@
  */
 class SingleProver {
 
+    std::string binariesFolderLoc; // where to find zkLogin and zkLogin.dat
+    std::string witnessBinaryLoc; // set in init
     mpz_t altBbn128r;
     std::unique_ptr<Groth16::Prover<AltBn128::Engine> > prover;
     std::unique_ptr<ZKeyUtils::Header> zkHeader;
     std::unique_ptr<BinFileUtils::BinFile> zKey;
 
 public: 
-    SingleProver(std::string zkeyFileName);
+    SingleProver(std::string zkeyFileName, std::string binariesFolderLoc);
     ~SingleProver();
     json startProve(std::string input);
 };
